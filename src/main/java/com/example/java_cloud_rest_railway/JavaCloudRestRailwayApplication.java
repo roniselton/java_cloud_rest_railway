3fc75a0c7d6a714a35e78d96e21ffa23f85af8bd
Roniselton <ronibarreto@gmail.com>
2023-10-22 15:00:46 -0300

    versao inicial

---
+++ src/main/java/com/example/java_cloud_rest_railway/JavaCloudRestRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_cloud_rest_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCloudRestRailwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCloudRestRailwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_cloud_rest_railway/JavaCloudRestRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_cloud_rest_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCloudRestRailwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
